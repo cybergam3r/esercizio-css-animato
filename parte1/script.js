cbcbe8839d69a329facfc894d5097ccdba090087
cybergam3r <giogio03.gr@gmail.com>
2024-11-28 17:22:49 +0100

    commit 2

--- parte1/script.js
+++ parte1/script.js
@@ -1,16 +1,21 @@
 const postits = document.querySelectorAll('.postit');
 postits.forEach(postit => {
-  const rotation = (Math.random() * 30 - 10);
-  postit.style.transform = `rotate(${rotation}deg)`; 
+    const random = randomdegree();
   const randomColor = getRandomColor();
+  postit.style.transform = `rotate(${random}deg)`; 
   postit.style.backgroundColor = randomColor;
   postit.addEventListener('mouseover', () => {
-    postit.style.transform = `rotate(0deg) scale(1.1)`; 
+    postit.style.transform = `rotate(${randomdegree()}deg) scale(1.2)`; 
   });
   postit.addEventListener('mouseout', () => {
-    postit.style.transform = `rotate(${rotation}deg)`;
+    postit.style.transform = `rotate(${randomdegree()}deg)`;
   });
 });
+function randomdegree(){
+    const rotation = (Math.random() * 30 - 10);
+    return rotation
+}
+
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
   let color = '#';
